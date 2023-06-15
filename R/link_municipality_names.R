293e6d4120867c703dc04cc2dab8b6bc367dbf9d
Eirik Berger <eirik.berger@nhh.no>
2023-06-15 11:11:34 +0200

    adds linking functionality (still dev)

---
+++ R/link_municipality_names.R
@@ -0,0 +1,68 @@
+#' Match Municipality Function
+#'
+#' This function takes a data.table with municipality details and matches a provided municipality name
+#' to its corresponding municipality number for a given year, using Jaro-Winkler string matching method.
+#' An optional county number can be specified.
+#'
+#' @param name The name of the municipality.
+#' @param year The year (optional). If not provided, the function will match the municipality
+#' irrespective of the year.
+#' @param county_number The number of the county (optional).
+#' @param threshold The minimum difference in string distance between the best and the second best match.
+#' @return The number of the municipality that matches the provided name and (if specified) year and
+#' county number, as well as the string distance and municipality name in a list.
+#' @examples
+#' \dontrun{
+#' df <- data.table(
+#'   municipality_name = c("Municipality1", "Municipality2", "Municipality3"),
+#'   municipality_number = c(1001, 1002, 1003),
+#'   year_created = c(1900, 1920, 1940),
+#'   year_stopped = c(1950, 1980, 2020)
+#' )
+#'
+#' name <- "Municipality1"
+#' year <- 1930
+#' county_num <- 10
+#'
+#' match_municipality(df, name, year, county_num)
+#' }
+#' @export
+#' @import data.table
+match_municipality <- function(name, year = NULL, county_number = NULL, threshold = 0.1) {
+  # Import municipality names
+  file_path <- system.file("data", "municipality_names.csv", package="NoRwayGeo")
+  df <- fread(file_path)
+
+  # Create a new column "county_number"
+  df[, county_number := floor(municipality_number / 100)]
+
+  # Compute the string distances
+  df[, dist := stringdist::stringdist(df$historiske_navn, name, method = "jw")]
+
+  # Filter based on the provided conditions
+  if(!is.null(year) & !is.null(county_number)){
+    result <- df[dist == min(dist) & year_created <= year & year_stopped >= year & county_number == county_number, .(municipality_number)]
+  } else if(!is.null(year)){
+    result <- df[dist == min(dist) & year_created <= year & year_stopped >= year, .(municipality_number)]
+  } else if(!is.null(county_number)){
+    result <- df[dist == min(dist) & county_number == county_number, .(municipality_number)]
+  } else {
+    result <- df[dist == min(dist), .(municipality_number, dist, historiske_navn)]
+  }
+
+  result <- unique(result)
+
+  print(result)
+
+  # Sort the result by string distance and check the second best match
+  result <- result[order(dist)]
+  if(nrow(result) > 1 && (result$dist[2] - result$dist[1]) < threshold){
+    return(paste("The second best match is too close to the best match for", name))
+  } else if(nrow(result) == 0){
+    return(paste("No municipality found for", name, "in the year", year))
+  } else {
+    return(list("municipality_name" = result$municipality_number[1],
+                "string_distance" = result$dist,
+                "name" = result$historiske_navn))
+  }
+}
---
+++ process_municipality_names.R
@@ -0,0 +1,41 @@
+pacman::p_load(data.table, readxl, stringr, janitor, zoo)
+
+###
+
+dta <- setDT(read_excel("inst/data/komnr.xlsx"))
+dta <- clean_names(dta)
+dta <- dta[,.(nr, navn, historiske_navn)]
+
+dta <- dta[, nr := na.locf(nr, na.rm = FALSE)]
+dta <- dta[, navn := na.locf(navn, na.rm = FALSE)]
+dta <- dta[, historiske_navn := na.locf(historiske_navn, na.rm = FALSE)]
+dta <- dta[!is.na(nr)]
+
+dta <- dta[str_detect(nr, "^[0-9]{3,4}")][, nr := str_match(nr,  "^[0-9]{3,4}")]
+dta <- dta[, navn := str_remove(navn,  "[★*]")]
+dta <- dta[, historiske_navn := str_remove(historiske_navn,  "[★*]")]
+
+dta <- dta[, historisk_detaljer := str_match(historiske_navn,  "(.*)\\s\\((.*?)\\)$")[,3]]
+dta <- dta[, historiske_navn := str_match(historiske_navn,  "(.*)\\s\\((.*?)\\)$")[,2]]
+
+tmp <- copy(dta)[, historiske_navn := navn]
+dta <- rbind(dta, tmp)
+rm(tmp)
+
+dta <- unique(dta)
+dta <- dta[!is.na(historiske_navn)]
+
+###
+
+# changes <- fread("inst/data/historical_municipalities.csv")
+# tmp1 <- changes[,.(.N), by = c("from", "year", "tidligere_kommunenavn")]
+# tmp2 <- changes[, .(year = min(year)), by = to]
+
+### Infer start and stop for municipalities and merge in ###
+
+###
+
+dta <- dta[, year_created := 1800][, year_stopped := 2020]
+setnames(dta, "nr", "municipality_number")
+fwrite(dta, "inst/data/municipality_names.csv")
+
